485d9c03a3de33cddaf3b3214143bd79299f051d
Caeca <71897616+ismailhafeez@users.noreply.github.com>
2023-01-28 16:04:06 +0530

    Updated main code.
    Number of variable is now no longer a limitation, special function to slice equation added.

--- AlgebraPy.py
+++ AlgebraPy.py
@@ -1,36 +1,46 @@
 class Algebra:
     def __init__(self):
-        pass
-        
-    def BasicSolution(self, question):
-        answer = int(question.split("=")[1])
-        products = [i for i in question.split("=")[0]]
-        for i in products:
+        self.answer = 0
+        self.reactants = []
+        self.vars = []
+    
+    def splitQuestion(self):
+        self.var = []
+        self.answer = int(self.question.split("=")[1])
+        self.reactants = [i for i in self.question.split("=")[0]]
+        for i in self.reactants:
             try:
-                products[products.index(i)] = int(i)
-            except Exception as e:
+                self.reactants[self.reactants.index(i)] = int(i)
+            except Exception:
                 if i.isalpha() == True:
-                    var = i
-        products.reverse()
-        while products != [var]:
-            for i in products:
-                n = products.index(i)
+                    self.var.append(i)
+        self.reactants.reverse()
+        
+    def Equation(self, question):
+        self.question = question
+        self.splitQuestion()
+        while self.reactants[0] not in self.var:
+            for i in self.reactants:
+                n = self.reactants.index(i)
                 try:
-                    if products[n+1] in ["+", "-", "/", "*"] and i != "x":
-                        if products[n+1] == "+":
-                            answer -= i
-                        if products[n+1] == "-":
-                            answer += i
-                        if products[n+1] == "/":
-                            answer *= i
-                        if products[n+1] == "*":
-                            answer /= i
-                        products[n] = ''
-                        products[n+1] = ''
-                except Exception as e:
+                    if self.reactants[n+1] in ["+", "-", "/", "*"] and i not in self.var:
+                        if self.reactants[n+1] == "+":
+                            self.answer -= i
+                        if self.reactants[n+1] == "-":
+                            self.answer += i
+                        if self.reactants[n+1] == "/":
+                            self.answer *= i
+                        if self.reactants[n+1] == "*":
+                            self.answer /= i
+                        self.reactants[n] = ''
+                        self.reactants[n+1] = ''
+                except Exception:
                     pass
-            products.remove('')
-        return answer
+            try:
+                self.reactants.remove('')
+            except Exception:
+                pass
+        return "".join(self.reactants) + "=" + str(self.answer)
         
 if __name__ == "__main__":
     pass
