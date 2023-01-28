1da6276e8c88f6d6384a9cb9163f9e88f93bb59b
Caeca <71897616+ismailhafeez@users.noreply.github.com>
2023-01-28 16:57:07 +0530

    Massive update to main code.
    Multiples and exponents of the variable can now be used (for example: 2x2 + 5 = 13)

--- AlgebraPy.py
+++ AlgebraPy.py
@@ -1,24 +1,29 @@
+import re
 class Algebra:
     def __init__(self):
         self.answer = 0
         self.reactants = []
-        self.vars = []
+        self.var = []
+        self.finalvar = []
     
     def splitQuestion(self):
-        self.var = []
         self.answer = int(self.question.split("=")[1])
-        self.reactants = [i for i in self.question.split("=")[0]]
+        try:
+            self.reactants = re.split('(\W+)', self.question.split("=")[0].replace(" ", ""))
+        except Exception:
+            pass
         for i in self.reactants:
             try:
                 self.reactants[self.reactants.index(i)] = int(i)
             except Exception:
-                if i.isalpha() == True:
+                if i not in ['+', '-', '/', '*']:
                     self.var.append(i)
         self.reactants.reverse()
         
-    def Equation(self, question):
+    def EquationOneVar(self, question):
         self.question = question
         self.splitQuestion()
+        print(self.reactants)
         while self.reactants[0] not in self.var:
             for i in self.reactants:
                 n = self.reactants.index(i)
@@ -40,6 +45,25 @@ class Algebra:
                 self.reactants.remove('')
             except Exception:
                 pass
+        try:
+            self.finalvar = re.split('(\d+)', self.reactants[0])
+            self.finalvar.remove('')
+            self.finalvar.remove('')
+            print(self.finalvar)
+            if self.finalvar[0].isalpha() == False:
+                print('a')
+                self.answer *= int(self.finalvar[0])
+                self.reactants[0] = self.reactants[0].replace(self.finalvar[0], '')
+            if self.finalvar[1].isalpha() == False:
+                print('b') 
+                self.answer **= 1/int(self.finalvar[1])
+                self.reactants[0] = self.reactants[0].replace(self.finalvar[1], '')
+            if self.finalvar[2].isalpha() == False:
+                print('c')
+                self.answer **= 1/int(self.finalvar[2])
+                self.reactants[0] = self.reactants[0].replace(self.finalvar[2], '')
+        except Exception as e:
+            print(e)
         return "".join(self.reactants) + "=" + str(self.answer)
         
 if __name__ == "__main__":
